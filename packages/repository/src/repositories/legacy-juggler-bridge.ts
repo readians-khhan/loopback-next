05c1bfe015676b5ce0d4ffb4a91697f77f881e76
derdeka <derdeka@blackslash.de>
2020-01-28 14:19:54 +0100

    feat(repository): hasManyThrough - fix

--- packages/repository/src/repositories/legacy-juggler-bridge.ts
+++ packages/repository/src/repositories/legacy-juggler-bridge.ts
@@ -23,16 +23,16 @@ import {
   BelongsToDefinition,
   createBelongsToAccessor,
   createHasManyRepositoryFactory,
+  createHasManyThroughRepositoryFactory,
   createHasOneRepositoryFactory,
   HasManyDefinition,
   HasManyRepositoryFactory,
+  HasManyThroughDefinition,
+  HasManyThroughRepositoryFactory,
   HasOneDefinition,
   HasOneRepositoryFactory,
   includeRelatedModels,
   InclusionResolver,
-  HasManyThroughDefinition,
-  HasManyThroughRepositoryFactory,
-  createHasManyThroughRepositoryFactory,
 } from '../relations';
 import {IsolationLevel, Transaction} from '../transaction';
 import {isTypeResolver, resolveType} from '../type-resolver';
@@ -269,7 +269,8 @@ export class DefaultCrudRepository<
     TargetEntiy extends Entity,
     TargetID,
     ThroughEntity extends Entity,
-    ThroughID
+    ThroughID,
+    ForeignKeyType
   >(
     relationName: string,
     targetRepositoryGetter: Getter<EntityCrudRepository<TargetEntiy, TargetID>>,
@@ -280,14 +281,15 @@ export class DefaultCrudRepository<
     TargetEntiy,
     TargetID,
     ThroughEntity,
-    ThroughID
+    ForeignKeyType
   > {
     const meta = this.entityClass.definition.relations[relationName];
     return createHasManyThroughRepositoryFactory<
       TargetEntiy,
       TargetID,
       ThroughEntity,
-      ThroughID
+      ThroughID,
+      ForeignKeyType
     >(
       meta as HasManyThroughDefinition,
       targetRepositoryGetter,
