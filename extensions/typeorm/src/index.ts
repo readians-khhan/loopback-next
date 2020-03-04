40b9586c53864bb14e57179f84a6155962f48066
Raymond Feng <enjoyjava@gmail.com>
2020-03-04 15:42:18 -0800

    feat(typeorm): add a compoennt for TypeORM integration

---
+++ extensions/typeorm/src/index.ts
@@ -0,0 +1,8 @@
+// Copyright IBM Corp. 2020. All Rights Reserved.
+// Node module: @loopback/typeorm
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+export * from './keys';
+export * from './typeorm.component';
+export * from './types';
